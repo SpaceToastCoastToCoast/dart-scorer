05a2c4f91de97f64b14f856d6d49d6ae35ec1e30
SpaceToastCoatToCoast <alloalloalan@gmail.com>
2016-09-28 09:28:04 -1000

    passed tests for non-bonus scoring

--- dart-scorer.js
+++ dart-scorer.js
@@ -1,5 +1,21 @@
 module.exports = function(scoreArray) {
+  if(scoreArray.length < 1) {
+    return 0;
+  }
   var result = 0;
+
+  var radiusToValues = scoreArray.map(function(radius) {
+    if(radius > 10) {
+      return 0;
+    } else if(radius < 5) {
+      return 10;
+    }
+    return 5;
+  });
+
+  result = radiusToValues.reduce(function(prev, curr) {
+    return prev + curr;
+  }, 0);
 
   return result;
 };
